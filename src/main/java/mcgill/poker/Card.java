c474a7ae8ae8d2da445508d897100428cb516342
ALarose <andrew.larose@mail.mcgill.ca>
2012-04-06 12:09:52 -0400

    Added methods to card class

--- src/main/java/mcgill/poker/Card.java
+++ src/main/java/mcgill/poker/Card.java
@@ -26,4 +26,44 @@ public class Card {
 		return suit;
 	}
 	
+	public String toString() {
+		String value;
+		String suit = null;
+		
+		switch (this.value) {
+			case 11:
+				value = "J";
+				break;
+			case 12:
+				value = "Q";
+				break;
+			case 13:
+				value = "K";
+				break;
+			case 14: 
+				value = "A";
+				break;
+			default:
+				value = String.valueOf(this.value);
+				break;
+		}
+		
+		switch (this.suit) {
+			case 0:
+				suit = "♣";
+				break;
+			case 1:
+				suit = "♦";
+				break;
+			case 2: 
+				suit = "♥";
+				break;
+			case 3: 
+				suit = "♠";
+				break;
+		}
+		
+		return value.concat(suit);
+	}
+	
 }
