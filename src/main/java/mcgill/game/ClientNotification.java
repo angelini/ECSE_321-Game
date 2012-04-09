9c5a3df3587bcd2641a47e57db8ba1fe57c58fd3
Alex Angelini <alex.louis.angelini@gmail.com>
2012-04-09 17:35:26 -0400

    FIXED - Possible timing conflict

--- src/main/java/mcgill/game/ClientNotification.java
+++ src/main/java/mcgill/game/ClientNotification.java
@@ -11,16 +11,21 @@ import redis.clients.jedis.JedisPubSub;
 public class ClientNotification {
 
 	private Gson gson;
-	private Jedis jedis;
+	private Jedis emit;
+	private Jedis subscribe;
 	private String session;
 	private String response;
 	
 	private class Listener extends JedisPubSub {
 
 		private ClientNotification clientNotify;
+		private String key;
+		private String message;
 		
-		public Listener(ClientNotification clientNotify) {
+		public Listener(ClientNotification clientNotify, String key, String message) {
 			this.clientNotify = clientNotify;
+			this.key = key;
+			this.message = message;
 		}
 		
 		public void onMessage(String channel, String message) {
@@ -34,7 +39,9 @@ public class ClientNotification {
 
 		public void onPUnsubscribe(String arg0, int arg1) {}
 
-		public void onSubscribe(String arg0, int arg1) {}
+		public void onSubscribe(String arg0, int arg1) {
+			this.clientNotify.emit.publish(this.key, this.message);
+		}
 
 		public void onUnsubscribe(String arg0, int arg1) {}
 		
@@ -43,17 +50,18 @@ public class ClientNotification {
 	public ClientNotification(String session) {
 		this.session = session;
 		this.gson = new Gson();
-		this.jedis = new Jedis(Config.REDIS_HOST, Config.REDIS_PORT, 0);
+		
+		this.emit = new Jedis(Config.REDIS_HOST, Config.REDIS_PORT);
+		this.subscribe = new Jedis(Config.REDIS_HOST, Config.REDIS_PORT, 0);
 	}
 	
 	public String getCommand(int[] limits) {
 		String n_key = Database.cat(Config.NOTIFICATIONS, Config.GET_COMMAND, this.session);
 		String c_key = Database.cat(Config.COMMAND, this.session);
 		
-		Listener listener = new Listener(this);
+		Listener listener = new Listener(this, n_key, this.gson.toJson(limits));
 		
-		this.jedis.publish(n_key, this.gson.toJson(limits));
-		this.jedis.subscribe(listener, c_key);
+		this.subscribe.subscribe(listener, c_key);
 		
 		return this.response;
 	}
@@ -61,22 +69,22 @@ public class ClientNotification {
 	public void sendHand(Map<String, Hand> hands) {
 		String n_key = Database.cat(Config.NOTIFICATIONS, Config.EMIT_HANDS, this.session);
 		HandsSerializable serializable = new HandsSerializable(hands);
-		this.jedis.publish(n_key, this.gson.toJson(serializable));
+		this.emit.publish(n_key, this.gson.toJson(serializable));
 	}
 	
 	public void sendUsers(User[] users) {
 		String n_key = Database.cat(Config.NOTIFICATIONS, Config.TABLE_USERS, this.session);
-		this.jedis.publish(n_key, this.gson.toJson(users));
+		this.emit.publish(n_key, this.gson.toJson(users));
 	}
 	
 	public void sendMessage(String chat_id) {
 		String n_key = Database.cat(Config.NOTIFICATIONS, Config.MESSAGE_REC, this.session);
-		this.jedis.publish(n_key, chat_id);
+		this.emit.publish(n_key, chat_id);
 	}
 	
 	public void potAndStatus(int[] current) {
 		String n_key = Database.cat(Config.NOTIFICATIONS, Config.POT_STATUS, this.session); 
-		this.jedis.publish(n_key, this.gson.toJson(current));
+		this.emit.publish(n_key, this.gson.toJson(current));
 	}
 	
 	public String getResponse() {
