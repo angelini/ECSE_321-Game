ecd61399b8bfb215d9810c5d864d6f8932143637
ALarose <andrew.larose@mail.mcgill.ca>
2012-04-09 17:33:31 -0400

    Test notification

--- src/main/java/mcgill/fiveCardStud/FiveCardStud.java
+++ src/main/java/mcgill/fiveCardStud/FiveCardStud.java
@@ -178,7 +178,7 @@ public class FiveCardStud implements Runnable {
 					int callAmount = getCallAmount() - currentPlayer.getAmountInPots();
 					
 					if (this.street == 2 && this.startingPlayer == index && i == 1) {
-						callAmount = this.bringIn;
+						callAmount = 666;//this.bringIn;
 					}
 					
 					int[] limits = {callAmount, betLimit};
